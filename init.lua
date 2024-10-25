1513339fad8486f74cdd9bc35b6a1ce605b7535b
monkofwst <bazarov.080706@yandex.ru>
2024-10-25 09:25:57 +0300

    config

--- init.lua
+++ init.lua
@@ -1,11 +1,17 @@
 --------------My settings -------------
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 vim.opt.encoding = "utf-8"
 vim.opt.fileencodings = {"utf-8", "cp1251", "latin1"}
 vim.opt.relativenumber = false
 vim.opt.number = true
 --vim.opt.colorcolumn = "79"
 vim.opt.clipboard = "unnamedplus"
+vim.o.swapfile = false
+-------- Keymaps --------
+-- Map 'd' to delete using the black hole register
+---vim.api.nvim_set_keymap('n', 'x', '"_d', { noremap = true, silent = true })
+---vim.api.nvim_set_keymap('v', 'x', '"_d', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-`>', ':Neotree toggle<CR>', { noremap = true, silent = true })
 -------------- Default settings ------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -19,6 +25,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+
+
+
+
 -------------- Installing packages -------------
 require("lazy").setup({"neovim/nvim-lspconfig",
                         "hrsh7th/nvim-cmp",
@@ -30,7 +41,23 @@ require("lazy").setup({"neovim/nvim-lspconfig",
                         "L3MON4D3/LuaSnip",
                         "p00f/clangd_extensions.nvim",
                         "sainnhe/gruvbox-material",
-                        "echasnovski/mini.nvim"})
+                        "echasnovski/mini.nvim",
+                        "nanotee/sqls.nvim",
+                        {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = { 
+                            "nvim-tree/nvim-web-devicons", 
+                            "MunifTanjim/nui.nvim"}},
+                        'nvim-lua/plenary.nvim',
+                        {
+                        "nvim-telescope/telescope.nvim",
+                        tag = '0.1.6',
+                        dependencies = { "nvim-lua/plenary.nvim" },
+                        },
+                        {
+                        "nvim-telescope/telescope-fzf-native.nvim",
+                        'preservim/nerdcommenter',
+                        build = "make",
+                        },
+})
 require("vim-options")
 local vim = vim
 --------- Themes config --------
@@ -44,7 +71,7 @@ vim.cmd('colorscheme gruvbox-material')
 vim.g.gruvbox_material_enable_italic_comment = 1  -- Enable italic comments
 
 vim.g.gruvbox_material_palette = "material"  -- Choose a palette: 'material', 'mix', or 'original'
-vim.g.gruvbox_material_transparent_background = 2  -- Set transparency level (0 to 2)
+vim.g.gruvbox_material_transparent_background = 0  -- Set transparency level (0 to 2)
 
 vim.cmd('syntax on')           -- Enable syntax highlighting
 vim.cmd('filetype plugin on')  -- Enable filetype detection and plugins
@@ -71,21 +98,16 @@ lspconfig.pyright.setup{
         }
     }
 }
-
-lspconfig.clangd.setup{
-    on_attach = function(client, bufnr)
-        -- Set up inlay hints
-        require("clangd_extensions.inlay_hints").setup_autocmd()
-        require("clangd_extensions.inlay_hints").set_inlay_hints()
-
-        -- Optional: Key mappings for toggling inlay hints
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lh', '<cmd>ClangdToggleInlayHints<CR>', { noremap = true, silent = true })
-    end,
-    capabilities = {
-        -- Add any specific capabilities here if needed
+require'lspconfig'.clangd.setup{
+    cmd = { "clangd", "--background-index" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = require('lspconfig.util').root_pattern("compile_commands.json", ".git"),
+    init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
     },
 }
-
 -------------- nvim-cmp --------------
 local cmp = require'cmp'
 
@@ -114,12 +136,11 @@ vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 ------------ mini config -----------
 local function require_mini()
   local mini_modules = {
-    "mini.ai",
+    -- "mini.ai",
     "mini.pairs",
-    "mini.comment",
+    -- "mini.comment",
     "mini.surround",
-    "mini.files",
-    "mini.statusline",
+    -- "mini.files",
     -- Add other mini modules as needed
   }
 
@@ -130,4 +151,83 @@ end
 
 -- Call the loader function
 require_mini()
+-------- SQL lsp config --------
+lspconfig.sqlls.setup {
+    cmd = { "sql-language-server", "up", "--method", "stdio" },
+    filetypes = { "sql", "mysql" },
+    root_dir = function()
+        return vim.loop.cwd() -- Set current working directory as root
+    end,
+}
+------- Neo Tree config -------
+require("neo-tree").setup {
+    close_if_last_window = false,
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = false,
+    follow_current_file = true,
+    view = {
+        width = 30,
+       side = "left",
+        auto_resize = true,
+    },
+}
+------- Telescope config -------
+require('telescope').setup{
+                defaults = {
+                    path_display = {"truncate"},
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+                            ["<C-j>"] = require("telescope.actions").move_selection_next,
+                            ["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist,
+                        },
+                    },
+                },
+            }
+require('telescope').load_extension('fzf')  -- Load fzf extension if installed
 
+local keymap = vim.keymap
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {desc = "Fuzz find files in cwd"})
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {desc = "Fuzzy find recent files"})
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", {desc = "Find string in cwd"})
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", {desc = "Find string under cursor in cwd"})
+--------- mini.statusline config ---------
+-- Define the function to get the file type icon
+local function get_filetype_icon()
+    local filename = vim.fn.expand('%:t')  -- Get current filename
+    local file_ext = vim.fn.expand('%:e')  -- Get file extension
+    local icon, _ = require'nvim-web-devicons'.get_icon(filename, file_ext, { default = true })  -- Get icon
+    return icon or ''  -- Return icon or empty string if none found
+end
+
+require('mini.statusline').setup({
+    content = {
+        active = function()
+            local filename = vim.fn.expand('%:t')  -- Get current file name
+            local git_branch = vim.b.gitsigns_status_dict and vim.b.gitsigns_status_dict.head or 'No Git'
+            local line_col = string.format('Ln %d, Col %d', vim.fn.line('.'), vim.fn.col('.'))
+
+            if git_branch == 'No Git' then
+                return string.format('%s | %s | %s',
+                    get_filetype_icon(),  -- Add file type icon
+                    filename,
+                    line_col
+                )
+            else
+                return string.format('%s | %s | %s | %s',
+                    get_filetype_icon(),  -- Add file type icon
+                    filename,
+                    git_branch,
+                    line_col
+                )
+            end
+        end,  -- Ensure this 'end' matches with 'active' function
+
+        inactive = function()
+            return 'Inactive Window'
+        end,  -- Add 'end' for inactive function correctly
+    },
+    use_icons = true,  -- Enable icons by default
+    set_vim_settings = true,  -- Set Vim's settings for statusline
+})
